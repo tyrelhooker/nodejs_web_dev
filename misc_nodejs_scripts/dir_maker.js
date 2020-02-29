1d09a8391a3e981cd3a2fd9d4764f9c7d901136c
TJ Hooker <tyrelhooker@gmail.com>
2020-02-29 17:04:58 -0500

    Add rmdir to dir_maker script to rename dir names.

--- misc_nodejs_scripts/dir_maker.js
+++ misc_nodejs_scripts/dir_maker.js
@@ -2,13 +2,30 @@
 
 const fs = require('fs');
 
-for (let i = 0; i <= 12; i++) {
-  fs.mkdir(`/home/tj/Documents/nodejs_web_dev/ch${i}`, { recursive: true }, (err) => {
+for (let i = 1; i <= 12; i++) {
+  fs.rmdir(`/home/tj/Documents/nodejs_web_dev/ch0${i}`, {recursive: true }, (err) => {
     if (err) {
-      return console.log(err);
+      console.log(err);
     }
-    console.log(`ch${i} directory was created.`)
+    console.log(`ch0${i} directory was removed.`);
   });
+
+  if (i < 10) {
+    fs.mkdir(`/home/tj/Documents/nodejs_web_dev/ch0${i}`, { recursive: true }, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(`ch0${i} directory was created.`)
+    });
+  } else {
+    fs.mkdir(`/home/tj/Documents/nodejs_web_dev/ch${i}`, { recursive: true }, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(`ch${i} directory was created.`)
+    });
+  }
+
 }
 
 
